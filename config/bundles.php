76161e05d6e959ba2acf7de78a26442187319137
sebastien deren laptop <sebastien.deren@gmail.com>
2023-06-27 15:41:43 +0200

    removing depreciated sensio bundles

--- config/bundles.php
+++ config/bundles.php
@@ -10,5 +10,4 @@ return [
     Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle::class => ['all' => true],
     Symfony\Bundle\MonologBundle\MonologBundle::class => ['all' => true],
     Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
-    Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle::class => ['all' => true],
 ];
