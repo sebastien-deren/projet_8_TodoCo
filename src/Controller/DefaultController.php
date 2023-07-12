9bc4de3c2ff7390ef4db33a8e34323db1344b81f
sebastien deren laptop <sebastien.deren@gmail.com>
2023-07-12 11:46:06 +0200

    adding methods for route

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -7,7 +7,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class DefaultController extends AbstractController
 {
-    #[Route(path: '/', name: 'homepage')]
+    #[Route(path: '/', name: 'homepage',methods:'GET')]
     public function indexAction(): \Symfony\Component\HttpFoundation\Response
     {
         return $this->render('default/index.html.twig');
