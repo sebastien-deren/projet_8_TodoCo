78b546449d72862127cd66b1244530dd8632053f
sebastien-deren <107032919+sebastien-deren@users.noreply.github.com>
2023-08-09 08:15:22 +0200

    Feature/implement tools integration (#5)
    * Adding travisCI integration
    adding sentry profiler
    trying to add codeclimate code coverage
    
    * typo on .travis.yml
    
    * test travis actions
    
    * changing travis
    
    * Trying to setup CI pipeline
    adding a symfony.yml setup for github workflow
    adding phpunit to our project
    
    * ready to upgrade recipes
    
    * creating the proper environment for tests
    
    * Feature/upgrading to attributes (#9)
    
    * changing annotation to attributes
    
    * correcting gitignore
    
    * Adding travisCI integration
    adding sentry profiler
    trying to add codeclimate code coverage
    
    * typo on .travis.yml
    
    * test travis actions
    
    * changing travis
    
    * Trying to setup CI pipeline
    adding a symfony.yml setup for github workflow
    adding phpunit to our project
    
    * ready to upgrade recipes
    
    * creating the proper environment for tests

--- tests/AppBundle/Controller/DefaultControllerTest.php
+++ tests/AppBundle/Controller/DefaultControllerTest.php
@@ -3,9 +3,15 @@
 namespace Tests\App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+use Symfony\Component\HttpClient\HttpClient;
 
 class DefaultControllerTest extends WebTestCase
 {
+    private HttpClient $client;
+    public function setup(){
+        $this->client = static::createClient();
+
+    }
     public function testIndex()
     {
         $client = static::createClient();
